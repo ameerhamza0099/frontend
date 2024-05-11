27df988c5282e5619af161fa0b19b451bc537b6f
Ameer Hamza <hamzae0099@gmail.com>
2024-05-11 18:40:52 +0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Picupload from './components/Picupload';
+
+function App() {
+  return (
+    <div className="App background">
+      <header className="App-header">
+      <Picupload/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Picupload.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Picupload = () => {
+  return (
+    <div>
+      <div class="card">
+  <div class="content">
+    <h2>Upload image</h2>
+    
+    <button>Upload</button>
+  </div>
+</div>
+
+    </div>
+  );
+}
+
+export default Picupload;
